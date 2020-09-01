4c2a13acea3115861c6c0720bf8eb9748a04ad0c
WebGatsby <36766289+WebGatsby@users.noreply.github.com>
2020-09-01 15:33:56 +0500

    Update manager.py

--- bots/manager.py
+++ bots/manager.py
@@ -13,8 +13,8 @@ print(datetime.datetime.now())
 while True:
     bets_to_place, last_bet_id = vprognoze.get_bet_info(browser, last_bet_id)
     for bet_to_place in range(0, len(bets_to_place)):
-        check_fuck = oddsfan.find_match_in_oddsfan(browser, bets_to_place[bet_to_place])
-        if check_fuck == -1:
+        check = oddsfan.find_match_in_oddsfan(browser, bets_to_place[bet_to_place])
+        if check == -1:
             continue
         pinnacle_bets = pinnacle.get_pinnacle_bets(browser)
         pinnacle_bet_list = pinnacle.find_pinnacle_way(bets_to_place[bet_to_place], pinnacle_bets)
